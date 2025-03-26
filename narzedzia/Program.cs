471c8ffe9d1c8ef667eea24472a73a4b88ca645b
admin <admin@ZMIEN-MNIE>
2025-03-26 15:33:09 +0100

    zmiany

--- narzedzia/Program.cs
+++ narzedzia/Program.cs
@@ -4,54 +4,63 @@ using System.Linq;
 
 class Program
 {
-
-
-    public static void Main(string[] args)
+    static string filePath = "magazyn.json";
+    static List<narzedzia> magazyn = new List<narzedzia>();
+    static void Main()
     {
-
-        Console.WriteLine(" Witaj w menadżerze narzędzi  ");
-        Console.WriteLine("1. Dodaj narzędzie ");
-        Console.WriteLine("2. Aktualizuj narzędzie ");
-        Console.WriteLine("3. Usuń narzędzie ");
-        Console.WriteLine("4. Zapisz narzędzia ");
-        Console.WriteLine("5. Wyświetl pojedyncze narzędzie ");
-        Console.WriteLine("6. Wyświetl wszystkie narzędzia ");
-        Console.WriteLine("7.  Wyjdź ");
-        Console.WriteLine(" Wybierz opcję : ");
-
-        string opcja = Console.ReadLine();
-
-        switch (opcja)
+        WczytajDane();
+        while (true)
         {
-            case "1":
-                DodajNarzedzie();
-                break;
-            case "2":
-                AktualizujNarzedzia();
-                break;
-            case "3":
-                UsunNarzedzia();
-                break;
-            case "4":
-                ZapiszNarzedzia();
-                break;
-            case "5":
-                Wyswietlpojedyncze();
-                break;
-            case "6":
-                Wyswietlwszystkie();
-                break;
-            case "7":
-                Console.WriteLine("Zamykanie programu.");
-                return;
-            default:
-                Console.WriteLine("Nieprawidłowy wybór");
-                break;
+            Console.Clear();
+            Console.WriteLine(" Witaj w menadżerze narzędzi  ");
+            Console.WriteLine("1. Dodaj narzędzie ");
+            Console.WriteLine("2. Aktualizuj narzędzie ");
+            Console.WriteLine("3. Usuń narzędzie ");
+            Console.WriteLine("4. Zapisz narzędzia ");
+            Console.WriteLine("5. Wyświetl pojedyncze narzędzie ");
+            Console.WriteLine("6. Wyświetl wszystkie narzędzia ");
+            Console.WriteLine("7.  Wyjdź ");
+            Console.WriteLine(" Wybierz opcję : ");
 
+            string opcja = Console.ReadLine();
 
+            switch (opcja)
+            {
+                case "1":
+                    DodajNarzedzie();
+                    break;
+                case "2":
+                    AktualizujNarzedzia();
+                    break;
+                case "3":
+                    UsunNarzedzia();
+                    break;
+                case "4":
+                    ZapiszNarzedzia();
+                    break;
+                case "5":
+                    Wyswietlpojedyncze();
+                    break;
+                case "6":
+                    Wyswietlwszystkie();
+                    break;
+                case "7":
+                    Console.WriteLine("Zamykanie programu.");
+                    return;
+                default:
+                    Console.WriteLine("Nieprawidłowy wybór");
+                    break;
+            }
         }
     }
+    public static void ZapiszDane()
+    {
 
+    }
+    public static void WczytajDane()
+    {
+
+    }
 
     public static void DodajNarzedzie() { 
     Console.WriteLine(" Podaj nazwę narzędzia :");
@@ -64,10 +73,16 @@ class Program
 
     }
         Console.WriteLine("Podaj cenę narzędzia : ");
-
-
-}
-
+       if (!decimal.TryParse(Console.ReadLine(), out decimal cena) || cena <= 0)
+        {
+            Console.WriteLine("Niepoprawna cena!");
+            return;
+        }
+        int id = magazyn.Count > 0 ? magazyn[^1].Id + 1 : 1;
+        magazyn.Add(new narzedzia { Id = id, Nazwa = nazwa, Ilosc = ilosc, Cena = cena });
+        ZapiszDane();
+        Console.WriteLine("Dodano narzędzie : !");
+    }
     public static void AktualizujNarzedzia() { 
 
     }
